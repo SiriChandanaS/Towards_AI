f8f547871f1d5f2f715ad15ea9415366e319b83e
Siri Chandana <142712752+SiriChandanaS@users.noreply.github.com>
2024-12-29 21:57:05 +0530

    Create Numpy.py

---
+++ Numpy.py
@@ -0,0 +1,9 @@
+import numpy as np
+arr=np.array([[1,2,6],[3,11,1]])
+numpyArr=np.array(arr)
+print(numpyArr)
+print(np.__version__)#version of Numpy
+print(arr[0,2])#indexing
+print(arr.ndim)#Number of Dimentions of Array
+print(arr[1:2])#slicing
+print(arr.itemsize)
